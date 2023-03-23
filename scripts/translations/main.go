5f82435847d74bf12a7b450b70d9326a57c99da6
Stanislav Chzhen <s.chzhen@adguard.com>
2023-03-23 16:27:01 +0300

    scripts: add locale update auto

--- scripts/translations/main.go
+++ scripts/translations/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
 	"flag"
@@ -11,6 +12,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"sync"
@@ -71,18 +73,29 @@ func main() {
 	conf, err := readTwoskyConf()
 	check(err)
 
+	command(uri, projectID, conf)
+}
+
+func command(uri *url.URL, projectID string, conf twoskyConf) {
 	switch os.Args[1] {
 	case "summary":
-		err = summary(conf.Languages)
+		err := summary(conf.Languages)
 		check(err)
 	case "download":
-		err = download(uri, projectID, conf.Languages)
+		err := download(uri, projectID, conf.Languages)
 		check(err)
 	case "unused":
-		err = unused()
+		err := unused()
 		check(err)
 	case "upload":
-		err = upload(uri, projectID, conf.BaseLangcode)
+		err := upload(uri, projectID, conf.BaseLangcode)
+		check(err)
+	// TODO!!: naming
+	case "update":
+		err := update(uri, projectID, conf.Languages, conf.BaseLangcode)
+		check(err)
+	case "upgrade":
+		err := upgrade(uri, projectID, conf.Languages, conf.BaseLangcode)
 		check(err)
 	default:
 		usage("unknown command")
@@ -186,10 +199,25 @@ func readLocales(fn string) (loc locales, err error) {
 
 // summary prints summary for translations.
 func summary(langs languages) (err error) {
+	sum, err := getSummary(langs)
+	if err != nil {
+		return fmt.Errorf("summary: %w", err)
+	}
+
+	for lang, f := range sum {
+		fmt.Printf("%s\t %6.2f %%\n", lang, f)
+	}
+
+	return nil
+}
+
+func getSummary(langs languages) (sum map[langCode]float64, err error) {
+	sum = make(map[langCode]float64)
+
 	basePath := filepath.Join(localesDir, defaultBaseFile)
 	baseLoc, err := readLocales(basePath)
 	if err != nil {
-		return fmt.Errorf("summary: %w", err)
+		return nil, fmt.Errorf("reading locales: %w", err)
 	}
 
 	size := float64(len(baseLoc))
@@ -206,15 +234,13 @@ func summary(langs languages) (err error) {
 		var loc locales
 		loc, err = readLocales(name)
 		if err != nil {
-			return fmt.Errorf("summary: reading locales: %w", err)
+			return nil, fmt.Errorf("summary: reading locales: %w", err)
 		}
 
-		f := float64(len(loc)) * 100 / size
-
-		fmt.Printf("%s\t %6.2f %%\n", lang, f)
+		sum[lang] = float64(len(loc)) * 100 / size
 	}
 
-	return nil
+	return sum, nil
 }
 
 // download and save all translations.  uri is the base URL.  projectID is the
@@ -244,12 +270,12 @@ func download(uri *url.URL, projectID string, langs languages) (err error) {
 		Timeout: 10 * time.Second,
 	}
 
-	var wg sync.WaitGroup
+	wg := &sync.WaitGroup{}
 	uriCh := make(chan *url.URL, len(langs))
 
 	for i := 0; i < numWorker; i++ {
 		wg.Add(1)
-		go downloadWorker(&wg, client, uriCh)
+		go downloadWorker(wg, client, uriCh)
 	}
 
 	for lang := range langs {
@@ -352,7 +378,7 @@ func unused() (err error) {
 
 	err = filepath.Walk(srcDir, func(name string, info os.FileInfo, err error) error {
 		if err != nil {
-			log.Info("accessing a path %q: %s", name, err)
+			log.Info("warning: accessing a path %q: %s", name, err)
 
 			return nil
 		}
@@ -461,4 +487,341 @@ func upload(uri *url.URL, projectID string, baseLang langCode) (err error) {
 	}
 
 	return nil
+}
+
+// TODO!!: docs
+func update(uri *url.URL, projectID string, langs languages, baseLang langCode) (err error) {
+	defer func() {
+		err = errors.Annotate(err, "update: %w")
+	}()
+
+	fmt.Println("Downloading the locales.")
+
+	err = download(uri, projectID, langs)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Checking if the English locale is up to date.")
+
+	ch, err := checkBaseLocale()
+	if err != nil {
+		return err
+	}
+
+	if ch == deletion {
+		err = handleDeletion(uri, projectID, baseLang)
+		if err != nil {
+			return err
+		}
+	}
+
+	if ch == addition {
+		err = handleAddition()
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Println("Checking if the release-blocker locales are fully translated")
+
+	err = checkReleaseBlockerLocales(langs)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Adding non-deletion changes.")
+
+	err = addAdditions()
+	if err != nil {
+		return err
+	}
+
+	if !ask("Proceed to remove deletion changes?") {
+		return nil
+	}
+
+	fmt.Println("Removing deletion changes.")
+
+	err = restoreLocales()
+	check(err)
+
+	fmt.Println(`Make sure that you're on fresh master.
+    git status
+Commit the changes.
+    git checkout -b 'upd-i18n'
+    git commit -m 'client: upd i18n'
+When merging the commit, only mention the GitHub issue 2643 if
+the update is before a final release as opposed to a beta one.`)
+
+	return nil
+}
+
+func handleDeletion(uri *url.URL, projectID string, baseLang langCode) (err error) {
+	err = restoreLocales()
+	if err != nil {
+		return err
+	}
+
+	if !ask("Upload the English locale?") {
+		return nil
+	}
+
+	err = upload(uri, projectID, baseLang)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Make sure that the new text has appeared on Crowdin.")
+
+	return nil
+}
+
+func handleAddition() (err error) {
+	// If there are additions, then someone probably unuploaded changes
+	// from unmerged branches.  This isn't critical, so just add this
+	// change.
+	fmt.Println("Adding english locale.")
+	err = addBaseLocale()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type change int
+
+const (
+	wrong change = iota - 2
+	deletion
+	none
+	addition
+)
+
+// checkBaseLocale returns change or error.  change is 0 if there is no
+// changes, 1 if there is additions, and -1 if there is deletions.
+func checkBaseLocale() (ch change, err error) {
+	path := filepath.Join(localesDir, defaultBaseFile)
+	cmd := exec.Command("git", "diff", "-U0", path)
+
+	buf := &bytes.Buffer{}
+	cmd.Stdout = buf
+
+	err = cmd.Run()
+	if err != nil {
+		return wrong, fmt.Errorf("checking base locale: %w", err)
+	}
+
+	if buf.Len() == 0 {
+		return none, nil
+	}
+
+	scanner := bufio.NewScanner(buf)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.HasPrefix(line, "+++") {
+			// continue
+		} else if strings.HasPrefix(line, "+") {
+			return addition, nil
+		}
+	}
+
+	return deletion, nil
+}
+
+func checkReleaseBlockerLocales(langs languages) (err error) {
+	sum, err := getSummary(langs)
+	if err != nil {
+		return fmt.Errorf("checking release blocker locales: %w", err)
+	}
+
+	blockers := []langCode{
+		"de",
+		"es",
+		"fr",
+		"it",
+		"ja",
+		"ko",
+		"pt-br",
+		"pt-pt",
+		"ru",
+		"zh-cn",
+		"zh-tw",
+	}
+
+	for _, b := range blockers {
+		if sum[b] != 100 {
+			return fmt.Errorf("locale %q is not fully translated", b)
+		}
+	}
+
+	return nil
+}
+
+func restoreLocales() (err error) {
+	cmd := exec.Command("git", "restore", localesDir)
+
+	buf := &bytes.Buffer{}
+	cmd.Stdout = buf
+
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("checking base locale: %w", err)
+	}
+
+	return nil
+}
+
+func addBaseLocale() (err error) {
+	path := filepath.Join(localesDir, defaultBaseFile)
+	cmd := exec.Command("git", "add", "-p", path)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("adding base locale: %w", err)
+	}
+
+	fmt.Printf("adding base locale: %q\n", path)
+
+	return nil
+}
+
+func addAdditions() (err error) {
+	adds, err := getAdditions()
+	if err != nil {
+		return fmt.Errorf("adding changes: %w", err)
+	}
+
+	for _, v := range adds {
+		path := filepath.Join(localesDir, v)
+
+		cmd := exec.Command("git", "add", "-p", path)
+
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		err = cmd.Run()
+		if err != nil {
+			return fmt.Errorf("adding locale %q: %w", path, err)
+		}
+	}
+
+	return nil
+}
+
+func getAdditions() (adds []string, err error) {
+	cmd := exec.Command("git", "diff", "-U0", localesDir)
+
+	buf := &bytes.Buffer{}
+	cmd.Stdout = buf
+
+	err = cmd.Run()
+	if err != nil {
+		return nil, fmt.Errorf("update running: %w", err)
+	}
+
+	scanner := bufio.NewScanner(buf)
+	key := ""
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.HasPrefix(line, "+++") {
+			i := strings.LastIndex(line, "/")
+
+			key = line[i:]
+		} else if strings.HasPrefix(line, "+") {
+			adds = append(adds, key)
+		}
+	}
+
+	return adds, nil
+}
+
+func upgrade(uri *url.URL, projectID string, langs languages, baseLang langCode) (err error) {
+	fmt.Println(`Do not upload anything until the feature branch that
+introduces the text is merged.  Once the branch is merged, proceed.`)
+
+	if !ask("Proceed?") {
+		return nil
+	}
+
+	defer func() {
+		err = errors.Annotate(err, "upgrade: %w")
+	}()
+
+	fmt.Println("Downloading the locales.")
+
+	err = download(uri, projectID, langs)
+	if err != nil {
+		fmt.Println("up grade download")
+		return err
+	}
+
+	fmt.Println("Checking if the English locale is up to date.")
+
+	ch, err := checkBaseLocale()
+	if err != nil {
+		return err
+	}
+
+	if ch == none {
+		fmt.Println("There is no changes.  Exiting.")
+
+		return nil
+	} else if ch == addition {
+		// If there are additions, then someone probably unuploaded changes
+		// from unmerged branches.  This isn't critical, so just add this
+		// change.
+		fmt.Println("Adding english locale.")
+		err = addBaseLocale()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if !ask("Proceed to restore locales?") {
+		return nil
+	}
+
+	err = restoreLocales()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Uploading english locale.")
+	err = upload(uri, projectID, baseLang)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ask reads line from STDIN if line is 'y' returns true, otherwise false.
+func ask(s string) (a bool) {
+	const q = "y/[n]: "
+
+	if s != "" {
+		fmt.Printf("%s ", s)
+	}
+	fmt.Printf("%s ", q)
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	line := scanner.Text()
+	line = strings.ToLower(line)
+
+	return line == "y"
 }
